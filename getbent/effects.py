6d038564446288e4c4516b9fe6d56643bb960077
Alex Cannan <alexfcannan@gmail.com>
2024-11-29 10:13:26 -0500

    add echo effect

--- getbent/effects.py
+++ getbent/effects.py
@@ -6,11 +6,14 @@ name=key1=value1:key2=value2:...
 import numpy as np
 
 
+SAMPLE_RATE = 44100
+
+
 def process_effect(data: np.ndarray, effect_string: str) -> np.ndarray:
     if "=" in effect_string:
         effect_name, arg_string = effect_string.split("=", maxsplit=1)
         kwargs = {
-            key: value
+            key: float(value)
             for key, value in (arg.split("=") for arg in arg_string.split(":"))
         }
     else:
@@ -27,3 +30,15 @@ def reverse(data: np.ndarray) -> np.ndarray:
 
 def invert(data: np.ndarray) -> np.ndarray:
     return -data
+
+
+def echo(data: np.ndarray, delay: float=1.0, decay: float=0.5) -> np.ndarray:
+    echo = data.copy()
+    delay_hop = int(delay * SAMPLE_RATE)
+    n_delays = data.shape[0] // delay_hop
+    for i in range(n_delays):
+        delay_i = echo[:data.shape[0] - i*delay_hop]
+        decay_coeff = decay ** i
+        delay_i *= decay_coeff
+        data[i*delay_hop:] += delay_i
+    return data
